c2827f5362bf2aab01e4140cd245f5e035815edd
Quadrilateral <amandacdulaney@gmail.com>
2022-01-31 18:19:44 -0500

    fixed error in local storage function

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,14 @@
 var today = moment();
 $("#currentDay").text(today.format("MMMM Do, YYYY"));
 
-//Save text to local storage
-var saveButton = document.getElementById("8am-button");
-var text = document.getElementById("8am");
-var textAppend = localStorage.getItem("calendartext");
+//Save text for each time slot to local storage
 
-text.textContent = textAppend;
-saveButton.addEventListener("click", function() {
-    localStorage.setItem("calendartext", textAppend);
+//8am
+var saveButton8 = document.getElementById("8am-button");
+var text8 = document.getElementById("8am");
+var textAppend8 = localStorage.getItem("calendartext8");
+
+saveButton8.addEventListener("click", function() {
+    localStorage.setItem("calendartext8", text8.value);
   }
 );
