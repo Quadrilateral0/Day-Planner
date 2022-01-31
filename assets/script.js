a9446bb1f26a634f1ccdd1ea79c158fae309d9b2
Quadrilateral <amandacdulaney@gmail.com>
2022-01-31 10:38:01 -0500

    inserted boostrap time blocks and save button, css markout, started eventListener

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,15 @@
 //Dynamic update of current date
 var today = moment();
-$("#currentDay").text(today.format("MMMM Do, YYYY, h:mm:ss a"));
+$("#currentDay").text(today.format("MMMM Do, YYYY"));
+
+//Save text to local storage
+var saveButton = document.getElementById("input-group-append");
+var text = document.getElementById("form-control");
+var textAppend = localStorage.getItem("calendartext");
+
+text.textContent = textAppend;
+saveButton.addEventListener("click", function() {
+    localStorage.setItem("calendartext", textAppend);
+   
+  }
+);
